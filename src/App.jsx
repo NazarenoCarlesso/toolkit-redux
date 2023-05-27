7a40b17dedb4d6ca49997f2cbbe0154549a2960a
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-05-27 11:01:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
